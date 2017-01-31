dc463bcdb5d12ce0a404510407de2ec819c9313c
Clementine <pirlot.clementine@gmail.com>
2017-01-31 07:30:32 +0100

    Update subtitlor_translated.sql

--- subtitlor_translated.sql
+++ subtitlor_translated.sql
@@ -29,15 +29,6 @@ CREATE TABLE `translated` (
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `translated`
---
-
-LOCK TABLES `translated` WRITE;
-/*!40000 ALTER TABLE `translated` DISABLE KEYS */;
-INSERT INTO `translated` VALUES (5,'password_presentation-new.srt'),(7,'2 Broke Girls - 02x09 - And The New Boss.LOL.English.HI.C.orig.Addic7ed.com-new.srt'),(9,'2 Broke Girls - 02x01 - And the Hidden Stash.LOL.English.HI.C.updated.Addic7ed.com-new.srt');
-/*!40000 ALTER TABLE `translated` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
